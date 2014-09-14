933116f83d47b8283b697519894d1356e4a81b8f
William Lyles <william@williamlyles.com>
2014-09-13 23:37:23 -0500

    bigger icons. Seriously, why are they so small?

--- easy-social-share.js
+++ easy-social-share.js
@@ -49,7 +49,7 @@ angular.module('td.easySocialShare', [])
         for (i = 0; i < shareLinks.length; i++) {
           var anchor = '';
           anchor += '<a href="'+ shareLinks[i].url + '"';
-          anchor += ' class="fa fa-'+shareLinks[i].network + square + '" target="_blank"';
+          anchor += ' class="fa fa-'+shareLinks[i].network + square + ' fa-lg" target="_blank"';
           anchor += '></a>';
           elem.append(anchor);
         }
