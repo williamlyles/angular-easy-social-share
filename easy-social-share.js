026a1407bbb01959bbaa2c5f9659a3c8a7781335
William Lyles <william@toaster>
2014-09-27 13:27:12 -0500

    re-indented code

--- easy-social-share.js
+++ easy-social-share.js
@@ -1,14 +1,18 @@
 'use strict';
 
+var ess_scripts = document.getElementsByTagName('script');
+var easySocialShareScript = ess_scripts[ess_scripts.length - 1];
+var easySocialSharePath = easySocialShareScript.substring(0, easySocialShareScript.lastIndexOf('/'));
+
 angular.module('td.easySocialShare', [])
-  .directive('shareLinks', ['$location', function ($location) {
+.directive('shareLinks', ['$location', function ($location) {
     return {
-      scope: {
+        scope: {
         shareLinks: '@',
         shareTitle: '@'
-      },
-      link: function (scope, elem, attrs) {
-        var i,
+        },
+        link: function (scope, elem, attrs) {
+            var i,
             sites = ['twitter', 'facebook', 'linkedin', 'google-plus', 'reddit'],
             theLink,
             links = scope.shareLinks.toLowerCase().split(','),
@@ -18,45 +22,39 @@ angular.module('td.easySocialShare', [])
             shareLinks = [],
             square = '';
 
-        elem.addClass('td-easy-social-share');
+            elem.addClass('td-easy-social-share');
 
-        // check if square icon specified
-        square = (attrs.shareSquare && attrs.shareSquare.toString() === 'true') ? '-square' : '';
+            // check if square icon specified
+            square = (attrs.shareSquare && attrs.shareSquare.toString() === 'true') ? '-square' : '';
 
-        // assign share link for each network
-        angular.forEach(links, function (key) {
-          key = key.trim();
+            // assign share link for each network
+            angular.forEach(links, function (key) {
+                    key = key.trim();
 
-          switch (key) {
-            case 'twitter':
-              theLink = 'http://twitter.com/intent/tweet?text=' + pageTitleUri + '%20' + pageLink;
-              break;
-            case 'facebook':
-              theLink = 'http://facebook.com/sharer.php?u=' + pageLink;
-              break;
-            case 'linkedin':
-              theLink = 'http://www.linkedin.com/shareArticle?mini=true&url=' + pageLink + '&title=' + pageTitleUri;
-              break;
-            case 'google-plus':
-              theLink = 'https://plus.google.com/share?url=' + pageLink;
-              break;
-            case 'reddit':
-              theLink = 'http://reddit.com/submit?url=' + pageLink  + '&title=' + pageTitleUri;
-              break;
-          }
+                    switch (key) {
+                        case 'twitter':
+                            theLink = 'http://twitter.com/intent/tweet?text=' + pageTitleUri + '%20' + pageLink;
+                        break;
+                        case 'facebook':
+                            theLink = 'http://facebook.com/sharer.php?u=' + pageLink;
+                        break;
+                        case 'linkedin':
+                            theLink = 'http://www.linkedin.com/shareArticle?mini=true&url=' + pageLink + '&title=' + pageTitleUri;
+                        break;
+                        case 'google-plus':
+                            theLink = 'https://plus.google.com/share?url=' + pageLink;
+                        break;
+                        case 'reddit':
+                            theLink = 'http://reddit.com/submit?url=' + pageLink  + '&title=' + pageTitleUri;
+                        break;
+                    }
 
-          if (sites.indexOf(key) > -1) {
-            shareLinks.push({network: key, url: theLink});
-          }
-        });
+                    if (sites.indexOf(key) > -1) {
+                        shareLinks.push({network: key, url: theLink});
+                    }
+            });
 
-        for (i = 0; i < shareLinks.length; i++) {
-          var anchor = '';
-          anchor += '<a href="'+ shareLinks[i].url + '"';
-          anchor += ' class="fa fa-'+shareLinks[i].network + square + ' fa-lg" target="_blank"';
-          anchor += '></a>';
-          elem.append(anchor);
+
         }
-      }
     };
-  }]);
+}]);
