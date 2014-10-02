8093510e8775057ae67d87252c16aaed8d577fe6
William Lyles <william@toaster>
2014-10-02 00:23:58 -0500

    Still kinda messy, but binding works now

--- easy-social-share.js
+++ easy-social-share.js
@@ -1,17 +1,13 @@
-'use strict';
-
-angular.module('td.easySocialShare', [])
-.directive('shareLinks', ['$location', function ($location) {
-    return {
+'use strict'; angular.module('td.easySocialShare', []) .directive('shareLinks', ['$location', function ($location) { return {
         scope: {
             shareLinks: '@',
-            shareTitle: '@'
+            shareTitle: '@',
+            shareUrl: '@'
         },
         link: function (scope, elem, attrs) {
             var i,
             sites = ['twitter', 'facebook', 'linkedin', 'google-plus', 'reddit'],
             theLink,
-            links = scope.shareLinks.toLowerCase().split(','),
             pageTitle = scope.shareTitle,
             shareLinks = [],
             square = '';
@@ -26,50 +22,57 @@ angular.module('td.easySocialShare', [])
             square = (attrs.shareSquare && attrs.shareSquare.toString() === 'true') ? '-square' : '';
 
             // assign share link for each network
-            angular.forEach(links, function (key) {
-                    key = key.trim();
+            scope.updateLinks = function(){
+                var shareLinks = [];
+                var links = scope.shareLinks.toLowerCase().split(',');
+                angular.forEach(links, function (key) {
+                        key = key.trim();
 
-                    switch (key) {
-                        case 'twitter':
-                            theLink = { url: 'http://twitter.com/intent/tweet',
-                                       options: {'text': scope.pageTitleUri + '%20' + scope.pageLink}};
-                        break;
-                        case 'facebook':
-                            theLink = { url:'http://facebook.com/sharer.php',
-                                        options:{'u': scope.pageLink}};
-                        break;
-                        case 'linkedin':
-                            theLink = { url: 'http://www.linkedin.com/shareArticle',
-                                        options:{    'mini':'true',
-                                                     'url':scope.pageLink,
-                                                     'title':scope.pageTitleUri}
-                                        };
-                        break;
-                        case 'google-plus':
-                            theLink = { url: 'https://plus.google.com/share',
-                                        options: {url:scope.pageLink}};
-                        break;
-                        case 'reddit':
-                            theLink = { url: 'http://reddit.com/submit',
-                                        options: {'url':scope.pageLink, 'title':scope.pageTitleUri}};
-                        break;
-                    }
+                        switch (key) {
+                            case 'twitter':
+                                theLink = { url: 'http://twitter.com/intent/tweet',
+                                           options: {'text': scope.shareTitle + '%20' + scope.shareUrl}};
+                            break;
+                            case 'facebook':
+                                theLink = { url:'http://facebook.com/sharer.php',
+                                            options:{'u': scope.shareUrl}};
+                            break;
+                            case 'linkedin':
+                                theLink = { url: 'http://www.linkedin.com/shareArticle',
+                                            options:{    'mini':'true',
+                                                         'url':scope.shareUrl,
+                                                         'title':scope.shareTitle}
+                                            };
+                            break;
+                            case 'google-plus':
+                                theLink = { url: 'https://plus.google.com/share',
+                                            options: {url:scope.shareUrl}};
+                            break;
+                            case 'reddit':
+                                theLink = { url: 'http://reddit.com/submit',
+                                            options: {'url':scope.shareUrl, 'title':scope.shareTitle}};
+                            break;
+                        }
 
-                    if (sites.indexOf(key) > -1) {
-                        shareLinks.push({network: key, url: theLink});
-                    }
-            });
-            scope.shareLinks = shareLinks; 
+                        if (sites.indexOf(key) > -1) {
+                            shareLinks.push({network: key, url: theLink});
+                        }
+                });
+                scope.socialLinks = shareLinks; 
+            };
+
+            scope.updateLinks();
+            scope.$watch('shareTitle', scope.updateLinks);
         
             scope.buildUrl = function(link){
-                var url = link.url + '?';
-                for(var key in link.options){
-                    url += key + '=' + link.options[key] + '&';
-                }                
+                var url = link.url.url + '?';
+                for(var key in link.url.options){
+                    url += key + '=' + encodeURIComponent(link.url.options[key]) + '&';
+                }     
                 return url;
-            }
+            };
 
         },
-        template: '<a ng-repeat="link in shareLinks track by $index" ng-class="\'fa fa-lg fa-\'+link.network"  target="_blank" ng-href="{{buildUrl(link)}}"></a>'
+        template: '<a ng-repeat="link in socialLinks" ng-class="\'fa fa-lg fa-\'+link.network"  target="_blank" ng-href="{{buildUrl(link)}}"></a>'
     };
 }]);
